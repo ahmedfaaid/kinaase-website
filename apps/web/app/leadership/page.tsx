2c23fdab4af836a574ba56fc45d0546162333314
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2025-03-22 23:38:10 +0000

    web: leadership page

---
+++ apps/web/app/leadership/page.tsx
@@ -0,0 +1,31 @@
+'use client';
+
+import AdvisoryBoard from '../../components/sections/advisory-board';
+import Hero from '../../components/sections/hero';
+import LeadershipCta from '../../components/sections/leadership-cta';
+import LeadershipTeam from '../../components/sections/leadership-team';
+import PageTransition from '../../components/ui/PageTransition';
+
+export default function Leadership() {
+  return (
+    <PageTransition>
+      <main className='min-h-screen'>
+        <Hero
+          title="Our <span class='text-primary-softer'>Leadership</span> Team"
+          subtitle='Meet the experienced professionals who guide our vision and strategy, bringing decades of expertise in corporate governance and technology.'
+          smallHero={true}
+          imageUrl='https://images.unsplash.com/photo-1556761175-5973dc0f32e7?ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80'
+        />
+
+        {/* Leadership Team */}
+        <LeadershipTeam />
+
+        {/* Advisory Board */}
+        <AdvisoryBoard />
+
+        {/* CTA Section */}
+        <LeadershipCta />
+      </main>
+    </PageTransition>
+  );
+}
