083c01205d8209642a0aa296300c7b684b50c845
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2025-03-22 16:38:07 +0000

    web: wrap content in query client, tooltip provider, add toaster and sonner to layout

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,5 +1,9 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { AnimatePresence } from 'framer-motion';
 import type { Metadata } from 'next';
+import { Toaster as Sonner } from '../components/ui/sonner';
+import { Toaster } from '../components/ui/toaster';
+import { TooltipProvider } from '../components/ui/tooltip';
 import './globals.css';
 import Template from './template';
 
@@ -7,6 +11,8 @@ export const metadata: Metadata = {
   title: 'Kinaase',
   description: 'Kinaase'
 };
+
+const queryClient = new QueryClient();
 
 export default function RootLayout({
   children
@@ -16,9 +22,15 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body>
-        <AnimatePresence mode='wait'>
-          <Template>{children}</Template>
-        </AnimatePresence>
+        <QueryClientProvider client={queryClient}>
+          <TooltipProvider>
+            <Toaster />
+            <Sonner />
+            <AnimatePresence mode='wait'>
+              <Template>{children}</Template>
+            </AnimatePresence>
+          </TooltipProvider>
+        </QueryClientProvider>
       </body>
     </html>
   );
