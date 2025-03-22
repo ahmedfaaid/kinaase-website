0a8bfeed631ecbba459d371fd9ab925dd6a670c8
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2025-03-22 21:26:50 +0000

    web: add sections to home page

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,7 +1,37 @@
+'use client';
+
+import AboutKinaase from '../components/sections/about-kinaase';
+import CTA from '../components/sections/cta';
+import Hero from '../components/sections/hero';
+import Services from '../components/sections/services';
+import Stats from '../components/sections/stats';
+import PageTransition from '../components/ui/PageTransition';
+
 export default function Home() {
   return (
-    <div>
-      <h1>Hello</h1>
-    </div>
+    <PageTransition>
+      <main className='min-h-screen'>
+        <Hero
+          title="Transforming Businesses <br/> with <span class='text-primary-softer'>Governance</span> & <span class='text-primary-softer'>Technology</span>"
+          subtitle="Kinaase provides comprehensive corporate governance and cutting-edge technology solutions to help businesses thrive in today's competitive landscape."
+          primaryButtonText='Explore Services'
+          primaryButtonLink='/corporate-services'
+          secondaryButtonText='Contact Us'
+          secondaryButtonLink='/contact'
+        />
+
+        {/* Services Overview Section */}
+        <Services />
+
+        {/* About Kinaase Section */}
+        <AboutKinaase />
+
+        {/* Stats Section */}
+        <Stats />
+
+        {/* CTA Section */}
+        <CTA />
+      </main>
+    </PageTransition>
   );
 }
