a8a61fc9ee4f66c91b8d990aa756d5475acc9575
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2025-03-22 23:52:55 +0000

    web: contact page

---
+++ apps/web/app/contact/page.tsx
@@ -0,0 +1,27 @@
+'use client';
+
+import ContactInfoForm from '../../components/sections/contact-info-form';
+import Hero from '../../components/sections/hero';
+import Map from '../../components/sections/map';
+import PageTransition from '../../components/ui/PageTransition';
+
+export default function Contact() {
+  return (
+    <PageTransition>
+      <main className='min-h-screen'>
+        <Hero
+          title="Get in <span class='text-primary-softer'>Touch</span>"
+          subtitle='Contact our team to learn more about our services or to discuss how we can help with your specific needs.'
+          smallHero={true}
+          imageUrl='https://images.unsplash.com/photo-1423666639041-f56000c27a9a?ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80'
+        />
+
+        {/* Contact Information and Form */}
+        <ContactInfoForm />
+
+        {/* Map Section */}
+        <Map />
+      </main>
+    </PageTransition>
+  );
+}
