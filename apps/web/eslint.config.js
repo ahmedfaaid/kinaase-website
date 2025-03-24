6e479f028949ff0d74a008ee048664b6d05afe4d
Ahmed Alhassan <ahmedfaaid.coding@gmail.com>
2025-03-24 16:35:36 +0000

    web: no unused variables ignore patterrn

--- apps/web/eslint.config.js
+++ apps/web/eslint.config.js
@@ -1,4 +1,22 @@
-import { nextJsConfig } from "@repo/eslint-config/next-js";
+import { nextJsConfig } from '@repo/eslint-config/next-js';
 
 /** @type {import("eslint").Linter.Config} */
-export default nextJsConfig;
+export default [
+  ...nextJsConfig,
+  {
+    rules: {
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          args: 'all',
+          argsIgnorePattern: '^_',
+          caughtErrors: 'all',
+          caughtErrorsIgnorePattern: '^_',
+          destructuredArrayIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          ignoreRestSiblings: true
+        }
+      ]
+    }
+  }
+];
